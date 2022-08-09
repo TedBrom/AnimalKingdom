5a8b17077565aec97f9ae586b1fb373f17d3eb52
Ed <edwardbromilow@live.co.uk>
2022-08-09 16:59:18 +0100

    coverage report changed

--- src/test/java/org/tests/CatTest.java
+++ src/test/java/org/tests/CatTest.java
@@ -8,28 +8,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CatTest
 {
     @Test
-    public void testGetCat()
+     void testGetCat()
     {
         Cat testCat = new Cat("jeff", "Meow", 7);
         assertEquals(7, testCat.getAge());
 
     }
     @Test
-    public void getName()
+     void getName()
     {
         Cat testCat1 = new Cat("harry", "Meow", 5);
         assertEquals("harry", testCat1.getName());
     }
 
     @Test
-    public void getTalking()
+     void getTalking()
     {
         Cat testCat = new Cat("harry", "Meow", 5);
         assertEquals("Meow", testCat.getTalking());
     }
 
     @Test
-    public void getAge()
+     void getAge()
     {
         Cat testCat1 = new Cat("harry", "Meow", 5);
         assertEquals(5, testCat1.getAge());
