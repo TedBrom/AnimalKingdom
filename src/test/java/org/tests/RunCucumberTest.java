7749f80dfe4e4fa81540c1d5b763c7950b43d0c0
Ed <edwardbromilow@live.co.uk>
2022-08-10 12:01:49 +0100

    added Cucumber (hopefully)

---
+++ src/test/java/org/tests/RunCucumberTest.java
@@ -0,0 +1,14 @@
+package org.tests;
+
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/cucumber")
+
+public class RunCucumberTest
+{
+    // stuff here
+}
