4c689ca4be03528679a8a7aa966a09568990cc43
Ed <edwardbromilow@live.co.uk>
2022-08-10 10:20:46 +0100

    smells removed

--- src/test/java/org/tests/PigeonTest.java
+++ src/test/java/org/tests/PigeonTest.java
@@ -1,6 +1,7 @@
 package org.tests;
 
 
+import org.example.Flying;
 import org.example.FlyingRat;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,28 +12,54 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
     void getPigeonAge()
     {
         FlyingRat newPigeon = new FlyingRat();
-        assertEquals(3, newPigeon.getAge());
+        assertEquals(3 , newPigeon.getAge());
     }
 
     @Test
     void getPigeonName()
     {
         FlyingRat newPigeon2 = new FlyingRat();
-        assertEquals("Polly", newPigeon2.getName());
+        assertEquals("Polly" , newPigeon2.getName());
 
     }
     @Test
     void getBirdColour()
     {
         FlyingRat newPigeon3 = new FlyingRat();
-        assertEquals("Grey", newPigeon3.getColour());
+        assertEquals("Grey" , newPigeon3.getColour());
     }
 
     @Test
     void getLifeStatus()
     {
         FlyingRat newPigeon4 = new FlyingRat();
-        assertEquals(false ,newPigeon4.isDead());
+        assertEquals(false , newPigeon4.isDead());
     }
 
+    @Test
+    void getColour()
+    {
+        FlyingRat newPigeon5 = new FlyingRat();
+        assertEquals("Grey", newPigeon5.getColour());
+    }
+
+    @Test
+    void getFlap()
+    {
+        FlyingRat newPigeon6 = new FlyingRat();
+        assertEquals(true , newPigeon6.flap());
+    }
+
+    @Test
+    void getGlide()
+    {
+        FlyingRat newPigeon7 = new FlyingRat();
+        assertEquals(true, newPigeon7.glide());
+    }
+    @Test
+    void getFly()
+    {
+        FlyingRat newPigeon8 = new FlyingRat();
+        assertEquals(true, newPigeon8.takeOff());
+    }
 }
